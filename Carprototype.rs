55d0b44b1a7dec5bacbc630a01ba00410525811e
Manikanta Lalam <manikantallm@gmail.com>
2025-02-26 15:01:41 +0530

    A car prototype while learning Structs and related topics

---
+++ Carprototype.rs
@@ -0,0 +1,40 @@
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(_color:String,_trans:Transmission,_conver:bool)->Car{
+    let car:Car = Car{
+        color:_color,
+        transmission:_trans,
+        convertible:_conver,
+        mileage:0,
+    };
+    car
+    
+}
+
+fn main(){
+    println!("The maincode is executed");
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
+
+}
